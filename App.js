ac16e28ef5b2241e4b1d00e207dfe4c5445648ea
naveenmobilefirst <naveen.mobilefirst@gmail.com>
2026-06-06 17:23:30 +0530

    updates

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-const parent = React.createElement(
-  "div",
-  { id: "partent" },
-  React.createElement(
-    "div",
-    { id: "child" },
+const parent = React.createElement("div", { id: "partent" }, [
+  React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1 tag"),
-  ),
-);
+    React.createElement("h2", {}, "I am h2 tag"), // for siblings in the same div we need to wrap them inside the array
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"), // for siblings in the same div we need to wrap them inside the array
+  ]),
+]);
 
 const heading = React.createElement(
   "h1",
